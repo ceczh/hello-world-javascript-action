2613010eb7735386551ebfa1da52ee78490b5720
Eathan <eathanchen@apjcorp.com>
2022-07-11 12:33:31 +0800

    My first action is ready

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  exec.exec('ll', ['/'])
+  //Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(exec.exec('ll', ['/']));
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
